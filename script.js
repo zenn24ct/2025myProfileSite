b3b21aad3f57a2d0a35bdfe2d17a4871f6656fef
Kkk0504 <kikka.takami@icloud.com>
2025-05-28 11:04:08 +0900

    タイピング風自己紹介消す

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-// タイピング風自己紹介
-const text = "Thank you for visiting my profile!";
-let i = 0;
-function typingEffect() {
-  if (i < text.length) {
-    document.getElementById("typed-text").innerHTML += text.charAt(i);
-    i++;
-    setTimeout(typingEffect, 60);
-  }
-}
-typingEffect();
+// // タイピング風自己紹介
+// const text = "Thank you for visiting my profile!";
+// let i = 0;
+// function typingEffect() {
+//   if (i < text.length) {
+//     document.getElementById("typed-text").innerHTML += text.charAt(i);
+//     i++;
+//     setTimeout(typingEffect, 60);
+//   }
+// }
+// typingEffect();
 
 // スクロール表示アニメーション
 const faders = document.querySelectorAll('.fade-in');
